224dd15e9fb15bf423b68504d07e56d99fe5e19a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-24 13:46:33 +0530

    Create adduser-nginx.sh

---
+++ adduser-nginx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 16 August 2019,
+# Description : Adding nginx user on rhel.
+
+# Note        : Please run this file with root privilages.
+
+# exits any line in the bash script fails.
+set -e
+
+# adding new user account.
+adduser -m -c "NGINX Server" nginx
+
+# deleting user password access.
+passwd -d nginx
+
+# adding new user to sudoers.
+chmod +w /etc/sudoers                                            && \
+echo "nginx       ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers && \
+chmod -w /etc/sudoers
+
+# copying existing .ssh folder to new user.
+cp -R $(pwd)/.ssh /home/nginx
+
+# changing owner to .ssh directory.
+chown -R nginx:nginx /home/nginx/.ssh
+
+# switching to new usesr.
+su - nginx
