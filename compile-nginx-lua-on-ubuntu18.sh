812d75059bcd25756a0fe0afc89788cb94e77cbd
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-09 13:38:39 +0530

    Update compile-nginx-lua-on-ubuntu18.sh

--- compile-nginx-lua-on-ubuntu18.sh
+++ compile-nginx-lua-on-ubuntu18.sh
@@ -57,41 +57,41 @@ rm   -rf $HOME/libressl.tar.gz                                                  
 mv       $HOME/libressl-* $HOME/libresslsrc
 
 # nginx downloading, extracting and compiling.
-curl -JL https://openresty.org/download/openresty-1.15.8.3.tar.gz -o $HOME/nginx.tar.gz && \
-tar  -xf $HOME/nginx.tar.gz -C $HOME                                                    && \
-rm   -rf $HOME/nginx.tar.gz                                                             && \
-mv       $HOME/openresty-* $HOME/nginxsrc                                               && \
-cd       $HOME/nginxsrc                                                                 && \
-./configure --prefix=$HOME/nginx                                                           \
-            --sbin-path=$HOME/nginx/bin/nginx                                              \
-            --conf-path=$HOME/nginx/conf/nginx.conf                                        \
-            --error-log-path=$HOME/nginx/log/error.log                                     \
-            --http-log-path=$HOME/nginx/log/access.log                                     \
-            --pid-path=$HOME/nginx/log/nginx.pid                                           \
-            --lock-path=$HOME/nginx/log/nginx.lock                                         \
-            --http-client-body-temp-path=$HOME/nginx/client_body_temp                      \
-            --http-fastcgi-temp-path=$HOME/nginx/fastcgi_temp                              \
-            --http-proxy-temp-path=$HOME/nginx/proxy_temp                                  \
-            --http-scgi-temp-path=$HOME/nginx/scgi_temp                                    \
-            --http-uwsgi-temp-path=$HOME/nginx/uwsgi_temp                                  \
-            --user=nginx                                                                   \
-            --group=nginx                                                                  \
-            --build=Ubuntu                                                                 \
-            --with-cpu-opt=generic                                                         \
-            --with-cc-opt='-O2'                                                            \
-            --with-ld-opt='-Wl,-rpath,/home/ubuntu/luajit/lib'                             \
-            --with-pcre=$HOME/pcresrc                                                      \
-            --with-pcre-jit                                                                \
-            --with-zlib=$HOME/zlibsrc                                                      \
-            --with-openssl=$HOME/libresslsrc                                               \
-            --with-openssl-opt=enable-ec_nistp_64_gcc_128                                  \
-            --with-openssl-opt=no-nextprotoneg                                             \
-            --with-openssl-opt=no-weak-ssl-ciphers                                         \
-            --with-openssl-opt=no-ssl3                                                     \
-            --with-http_ssl_module                                                      && \
-make                                                                                    && \
+curl -JL https://openresty.org/download/openresty-1.15.8.3.tar.gz -o $HOME/nginx.tar.gz         && \
+tar  -xf $HOME/nginx.tar.gz -C $HOME                                                            && \
+rm   -rf $HOME/nginx.tar.gz                                                                     && \
+mv       $HOME/openresty-* $HOME/nginxsrc                                                       && \
+cd       $HOME/nginxsrc                                                                         && \
+./configure --prefix=$HOME/nginx                                                                   \
+            --sbin-path=$HOME/nginx/bin/nginx                                                      \
+            --conf-path=$HOME/nginx/conf/nginx.conf                                                \
+            --error-log-path=$HOME/nginx/log/error.log                                             \
+            --http-log-path=$HOME/nginx/log/access.log                                             \
+            --pid-path=$HOME/nginx/log/nginx.pid                                                   \
+            --lock-path=$HOME/nginx/log/nginx.lock                                                 \
+            --http-client-body-temp-path=$HOME/nginx/client_body_temp                              \
+            --http-fastcgi-temp-path=$HOME/nginx/fastcgi_temp                                      \
+            --http-proxy-temp-path=$HOME/nginx/proxy_temp                                          \
+            --http-scgi-temp-path=$HOME/nginx/scgi_temp                                            \
+            --http-uwsgi-temp-path=$HOME/nginx/uwsgi_temp                                          \
+            --user=nginx                                                                           \
+            --group=nginx                                                                          \
+            --build=Ubuntu                                                                         \
+            --with-cpu-opt=generic                                                                 \
+            --with-cc-opt='-static -static-libgcc -fPIE'                                           \
+            --with-ld-opt='-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-rpath,/home/ubuntu/luajit/lib' \
+            --with-pcre=$HOME/pcresrc                                                              \
+            --with-pcre-jit                                                                        \
+            --with-zlib=$HOME/zlibsrc                                                              \
+            --with-openssl=$HOME/libresslsrc                                                       \
+            --with-openssl-opt=enable-ec_nistp_64_gcc_128                                          \
+            --with-openssl-opt=no-nextprotoneg                                                     \
+            --with-openssl-opt=no-weak-ssl-ciphers                                                 \
+            --with-openssl-opt=no-ssl3                                                             \
+            --with-http_ssl_module                                                              && \
+make                                                                                            && \
 make install
 
 # cleaning process.
 rm -rf $HOME/{luajit,luajitsrc,nginxsrc,libresslsrc,pcresrc,zlibsrc}
-rm -rf $HOME/nginx/{COPYRIGHT,resty.index,nginx,pod,site}
+rm -rf $HOME/nginx/{COPYRIGHT,resty.index,pod,site}
