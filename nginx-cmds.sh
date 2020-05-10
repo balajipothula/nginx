fb963d940cb6118d87ed9b3115a18123fbe9c1b2
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-10 11:56:52 +0530

    Update nginx-cmds.sh

--- nginx-cmds.sh
+++ nginx-cmds.sh
@@ -15,3 +15,18 @@ sudo ./nginx -s reload
 
 # Reopen log files.
 sudo ./nginx -s reopen
+
+# daemon file path.
+/lib/systemd/system/nginx.service
+
+# starting nginx service as daemon.
+sudo systemctl start nginx.service
+
+# enabling nginx service as daemon.
+sudo systemctl enable nginx.service
+
+# checking nginx started at boot time.
+sudo systemctl is-enabled nginx.service
+
+# checking nginx running or not.
+sudo systemctl status nginx.service
