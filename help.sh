111706fe985dc468c206b8df8bde46edc3a6efce
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-16 17:55:30 +0530

    Update help.sh

--- help.sh
+++ help.sh
@@ -3,3 +3,6 @@ luajit -b -g todaylib.lua todaylib.o
 
 # Test Today Lua.
 luajit test_today.lua
+
+# Compiling LuaJit Byte Code in to lualib.
+luajit -bg todaylib.lua $HOME/nginx/lualib/todaylib.ljbc
