d2d863adbe8aab418df20b49c2efa890b2880f26
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-30 15:31:25 +0530

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,9 +1,12 @@
 # Build Lua Object File.
 # --with-ld-opt="-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-rpath,/home/ubuntu/luajit/lib /home/ubuntu/todaylib.o"
-# --with-ld-opt="/home/ubuntu/addlib.o /home/ubuntu/sublib.o /home/ubuntu/mullib.o /home/ubuntu/divlib.o"
+# --with-ld-opt="/home/ubuntu/addlib.o /home/ubuntu/sublib.o /home/ubuntu/mullib.o /home/ubuntu/divlib.o" 
 
 # aRCHIVE all oBJECT files (eg. addlib.o, sublib.o, mullib.o and divlib.o)
 ar rcus libcalc.a *.o
+
+# if we specify the lib path then no need of lib prefix for calc archive.
+--with-ld-opt="-L/home/ubuntu/liblua -Wl,--whole-archive -lcalc -Wl,--no-whole-archive"
 
 # compile lua module files to .o object files,
 # containing the exported bytecode data.
