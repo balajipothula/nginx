4194732b5e3fc90d064c28304a61a006c9f73b96
BALAJI POTHULA <balan.pothula@gmail.com>
2020-05-30 14:25:54 +0530

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,6 +1,12 @@
 # Build Lua Object File.
 # --with-ld-opt="-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-rpath,/home/ubuntu/luajit/lib /home/ubuntu/todaylib.o"
 # --with-ld-opt="/home/ubuntu/addlib.o /home/ubuntu/sublib.o /home/ubuntu/mullib.o /home/ubuntu/divlib.o"
+
+# aRCHIVE all oBJECT files (eg. addlib.o, sublib.o, mullib.o and divlib.o)
+ar rcus libcalc.a *.o
+
+# compile lua module files to .o object files,
+# containing the exported bytecode data.
 luajit -b -g todaylib.lua todaylib.o
 
 # Test Today Lua.
