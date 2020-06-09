e91bf7fa79e90e60197c3d865d518fab0ef3479d
BALAJI POTHULA <balan.pothula@gmail.com>
2020-06-09 19:51:23 +0530

    Update dns_query.lua

--- dns_query.lua
+++ dns_query.lua
@@ -1,7 +1,7 @@
 local cjson    = require("cjson")
 local resolver = require("resty.dns.resolver")
 
-local r, err = resolver:new{ nameservers = { "8.8.8.8" } }
+local r, err = resolver:new{ nameservers = { "8.8.8.8", { "8.8.4.4", 53 } }, retrans = 5, timeout = 2000, no_recurse = false }
 
 if not r then
   ngx.say("failed to instantiate resolver: ", err)
