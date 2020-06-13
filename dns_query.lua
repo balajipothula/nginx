52b5fb667d3408e62696281cc0dc13ca0a432ac6
BALAJI POTHULA <balan.pothula@gmail.com>
2020-06-13 20:19:32 +0530

    Update dns_query.lua

--- dns_query.lua
+++ dns_query.lua
@@ -1,14 +1,14 @@
 local cjson    = require("cjson")
 local resolver = require("resty.dns.resolver")
 
-local r, err = resolver:new{ nameservers = { "8.8.8.8", { "8.8.4.4", 53 } }, retrans = 5, timeout = 2000, no_recurse = false }
+local r, err = resolver:new{ nameservers = { "8.8.8.8", { "8.8.4.4", 53 }, "1.1.1.1" }, retrans = 5, timeout = 2000, no_recurse = false }
 
 if not r then
   ngx.say("failed to instantiate resolver: ", err)
   return
 end
 
-local ans, err = r:tcp_query("www.google.com", { qtype = r.TYPE_A })
+local ans, err = r:tcp_query("blitzsystems.in", { qtype = r.TYPE_TXT })
 if not ans then
   ngx.say("failed to query: ", err)
   return
